e9dc8cd72d26285938fd1753334c6faaf976fbb1
Henrique <henriquelorenzini33@gmail.com>
2019-05-15 13:24:52 -0300

    tela de home page criada

--- app/src/main/java/com/example/starwarsapp/MainActivity.java
+++ app/src/main/java/com/example/starwarsapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.starwarsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tela_inicial);
+        setContentView(R.layout.loading_page);
+    public void onClick (Button);
+         buttonStart =  findViewById(R.id.buttonStart);
+
+         buttonStart.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent it = new Intent(MainActivity.this, homePage.this);
+                 startActivity(it);
+             }
+         });
     }
 }
---
+++ app/src/main/java/com/example/starwarsapp/homePage.java
@@ -0,0 +1,4 @@
+package com.example.starwarsapp;
+
+public class homePage extends MainActivity {
+}
